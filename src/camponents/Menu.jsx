cd350be49438c4de8f5637fe9081039330e1c4fd
Hamidreza Yavarzadeh <hamid.yavarzadeh2002@gmail.com>
2023-11-26 20:08:11 +0330

    feat : add camponent Menu

---
+++ src/camponents/Menu.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Pizaa from "./Pizaa";
+
+function Menu() {
+  return (
+    <div>
+      <main className="menu">
+        <h2>our menu</h2>
+        <Pizaa />
+      </main>
+    </div>
+  );
+}
+
+export default Menu;
