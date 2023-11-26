05c5503fcaf0a8e68b103fb117e8a517c26ce2a8
Hamidreza Yavarzadeh <hamid.yavarzadeh2002@gmail.com>
2023-11-26 20:05:53 +0330

    feat : create App file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Footer from "./camponents/Footer";
+import Header from "./camponents/Header";
+import Menu from "./camponents/Menu";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header />
+      <Menu />
+      <Footer />
     </div>
   );
 }
