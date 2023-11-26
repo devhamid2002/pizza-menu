555b16fcb88d96052c4492bacf475603445c2d8e
Hamidreza Yavarzadeh <hamid.yavarzadeh2002@gmail.com>
2023-11-26 20:09:17 +0330

    feat : add camponent Pizza

---
+++ src/camponents/Pizza.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Pizaa() {
+  return (
+    <div>
+      <div className="pizza">
+        <img
+          src="https://images.pexels.com/photos/2619970/pexels-photo-2619970.jpeg?auto=compress&cs=tinysrgb&w=1600"
+          alt=""
+        />
+        <div>
+          <h2>Vegetable Pizza</h2>
+          <p>KNSGN;SFLG</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Pizaa;
