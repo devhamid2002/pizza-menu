922e13ac4822de53c14e693cee2527745bbb38dc
Hamidreza Yavarzadeh <hamid.yavarzadeh2002@gmail.com>
2023-11-26 20:07:58 +0330

    feat : add camponent

---
+++ src/camponents/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div>
+      <header className="header">
+        <h1>Fast Creat Pizza co.</h1>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
